274c69b0c0a5bb48797baf2cf9406d4f35fa4f1f
Eric Chung <rchung.eric@gmail.com>
2018-01-08 21:08:26 +1100

    add files

---
+++ src/components/search_bar.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+// needed because when webpack transpiles JSX to React code,
+// browser needs React file to access React.CreateElement
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
+  render() {
+    return (
+      <div className="search-bar">
+        <input
+          value = {this.state.term}
+          onChange={event => this.onInputChange(event.target.value)} />
+      </div>
+    );
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
+}
+
+export default SearchBar;
